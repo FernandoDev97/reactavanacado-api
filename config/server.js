43a471e34795456569280fa392587fa0fbe9cf30
FernandoDev97 <l.fernando.dev@gmail.com>
2022-04-21 10:40:35 -0300

    fist commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bfc8036421505092ca89cac3abe4ac37'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9b0c7455-1747-4dec-85bc-a9b13802a937'
+};
